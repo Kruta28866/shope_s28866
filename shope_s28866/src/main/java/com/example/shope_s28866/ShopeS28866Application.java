ea6efdee247d8f98822f27fdd01105b824af64ce
s28866 <116429757+Kruta28866@users.noreply.github.com>
2024-02-01 18:07:52 +0100

    Pierwszy Commit Dodanie plików z springa

---
+++ shope_s28866/src/main/java/com/example/shope_s28866/ShopeS28866Application.java
@@ -0,0 +1,13 @@
+package com.example.shope_s28866;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeS28866Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeS28866Application.class, args);
+	}
+
+}
---
+++ shope_s28866/src/test/java/com/example/shope_s28866/ShopeS28866ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shope_s28866;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeS28866ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
